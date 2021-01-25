d652f7a6795326ad2ef02ef1a711f1c257efd9c6
huyduong155 <nhathuy.155@gmail.com>
2021-01-25 09:44:28 +0700

    chapter 7

--- app/jobs/application_job.rb
+++ app/jobs/application_job.rb
@@ -4,5 +4,9 @@ class ApplicationJob < ActiveJob::Base
 
   # Most jobs are safe to ignore if the underlying records are no longer
 
+  # Most jobs are safe to ignore if the underlying records are no longer
+
+  # Most jobs are safe to ignore if the underlying records are no longer available
+
   # discard_on ActiveJob::DeserializationError
 end
