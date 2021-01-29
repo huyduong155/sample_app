762bb6a8c8d6f6511dcb8a1258b720cff090ce5f
huyduong155 <nhathuy.155@gmail.com>
2021-01-29 12:51:37 +0700

    chapter 11

---
+++ config/application.rb
@@ -0,0 +1,24 @@
+require_relative 'boot'
+
+require 'rails/all'
+
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(*Rails.groups)
+
+module SampleApp
+  class Application < Rails::Application
+    # Initialize configuration defaults for originally generated Rails version.
+    config.load_defaults 6.0
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales',
+        '**', '*.{rb,yml}')]
+    config.i18n.available_locales = [:en, :vi]
+    config.i18n.default_locale = :vi
+    config.before_configuration do
+      env_file = File.join(Rails.root, 'config', 'application.yml')
+      YAML.load(File.open(env_file)).each do |key, value|
+        ENV[key.to_s] = value
+      end if File.exists?(env_file)
+    end
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,16 @@
+Rails.application.routes.draw do
+  scope "(:locale)", locale: /en|vi/ do
+    resources :microposts
+    resources :users
+    resources :account_activations, only: :edit
+    root to: 'static_pages#home'
+    get 'static_pages/help'
+    get "/signup", to: "users#new"
+    post "/signup", to: "users#create"
+    resources :users, only: %i(new create)
+    get "/login", to: "sessions#new"
+    post "/login", to: "sessions#create"
+    delete "/logout", to: "sessions#destroy"
+    get "/account_activations", to: "account_activations#edit"
+  end
+end
