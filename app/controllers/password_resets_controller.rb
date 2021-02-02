0a1c04885a86c608075d7eadb5c5ec0fa04b4436
huyduong155 <nhathuy.155@gmail.com>
2021-02-02 11:45:36 +0700

    chapter 12

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -1,4 +1,7 @@
 class PasswordResetsController < ApplicationController
+  before_action :get_user, only: [:edit, :update]
+  before_action :valid_user, only: [:edit, :update]
+
   def new
   end
 
@@ -10,11 +13,36 @@ class PasswordResetsController < ApplicationController
     if @user
       @user.create_reset_digest
       @user.send_password_reset_email
-      flash[:info] = "Email sent with password reset instructions"
+      flash[:info] = t("email_sent_with_password_reset_instructions")
       redirect_to root_url
     else
-      flash.now[:danger] = "Email address not found"
+      flash.now[:danger] = t("email_address_not_found")
       render :new
     end
+  end
+
+  def update
+    if user_params[:password].empty?
+      flash[:warning] = t("please_enter_password")
+    elsif @user.update_attributes user_params
+      redirect_to login_path
+      flash[:success] = t("updated")
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit :password, :password_confirmation
+  end
+
+  def get_user
+    @user = User.find_by email: params[:email]
+  end
+  def valid_user
+    return if (@user && @user.activated? && @user.authenticated?(:reset, params[:id]))
+    redirect_to root_url
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,23 @@
 Rails.application.routes.draw do
-  get 'password_resets/new'
-  get 'password_resets/edit'
-  root to: 'static_pages#home'
-  get 'static_pages/help'
+
+
   scope "(:locale)", locale: /en|vi/ do
     resources :microposts
     resources :users
     resources :account_activations, only: :edit
+    root to: 'static_pages#home'
+    get 'static_pages/help'
+    get 'password_resets/new'
+    get 'password_resets/edit'
+    get "/signup", to: "users#new"
+    post "/signup", to: "users#create"
+    resources :users, only: %i(new create)
+    get "/login", to: "sessions#new"
+    post "/login", to: "sessions#create"
+    delete "/logout", to: "sessions#destroy"
+    get "/account_activations", to: "account_activations#edit"
+    resources :password_resets, only: [:new, :create, :edit, :update]
   end
-  get "/signup", to: "users#new"
-  post "/signup", to: "users#create"
-  resources :users, only: %i(new create)
-  get "/login", to: "sessions#new"
-  post "/login", to: "sessions#create"
-  delete "/logout", to: "sessions#destroy"
-  get "/account_activations", to: "account_activations#edit"
-  resources :password_resets, only: [:new, :create, :edit, :update]
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
